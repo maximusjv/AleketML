0e1d491c2708883f902ecd307fc98d832ab37267
maximusjv <maksimus.vinokur@gmail.com>
2024-10-18 21:41:29 +0300

    aleket_dataset documented

--- aleket_dataset.py
+++ aleket_dataset.py
@@ -24,6 +24,7 @@ def download_dataset(save_dir: str, patched_dataset_gdrive_id: str = ""):
     Args:
         save_dir: The directory to save the dataset.
         patched_dataset_gdrive_id: GoogleDrive id to download the dataset from.
+
     Returns:
         The path to the saved dataset directory.
     """
@@ -37,11 +38,16 @@ def download_dataset(save_dir: str, patched_dataset_gdrive_id: str = ""):
 class AleketDataset(Dataset):
     """Custom dataset for Aleket images and annotations.
 
+    Args:
+        dataset_dir: Directory containing the 'imgs' folder and 'dataset.json'.
+        img_size: Size to resize the image to.
+        augmentation: Optional torchvision transforms to apply to the data.
+
     Attributes:
         NUM_TO_CLASSES: Mapping of numerical labels to class names.
         CLASSES_TO_NUM: Mapping of class names to numerical labels.
     """
-
+    
     CLASSES_TO_NUM = {"background": 0, "healthy": 1, "not healthy": 2}
     NUM_TO_CLASSES = {0: "background", 1: "healthy", 2: "not healthy"}
 
@@ -51,13 +57,6 @@ class AleketDataset(Dataset):
         img_size: int,
         augmentation: Optional[v2.Transform] = None
     ) -> None:
-        """Initializes the AleketDataset.
-
-        Args:
-            dataset_dir: Directory containing the 'imgs' folder and 'dataset.json'.
-            img_size: Size to resize the image to.
-            augmentation: Optional torchvision transforms to apply to the data.
-        """
         self.img_dir = os.path.join(dataset_dir, "imgs")
         
         with open(os.path.join(dataset_dir, "dataset.json"), "r") as annot_file:
@@ -79,6 +78,11 @@ class AleketDataset(Dataset):
         print(f"Dataset loaded from {dataset_dir}")
 
     def by_full_images(self) -> dict[str, list[int]]:
+        """Groups image indices by their corresponding full image ID.
+        Returns:
+            dict[str, list[int]]: A dictionary mapping full image IDs to lists of 
+                                  corresponding image indices in the dataset.
+        """
         by_full_images = {}
         for idx, name in enumerate(self.images):
             full_image_id = name.split('_')[0]
@@ -88,9 +92,20 @@ class AleketDataset(Dataset):
         return by_full_images
 
     def __len__(self):
+        """Returns the total number of images in the dataset."""
         return len(self.images)
  
     def __getitem__(self, idx: int):
+        """
+        Retrieves an image and its corresponding target annotations.
+
+        Args:
+            idx (int): The index of the image to retrieve.
+
+        Returns:
+            tuple: A tuple containing the image as a torchvision.tv_tensors.Image object 
+                   and a dictionary of target annotations.
+        """
         image_id = self.images[idx]  
         
         annots = self.dataset[image_id]
@@ -99,7 +114,6 @@ class AleketDataset(Dataset):
         img_path = os.path.join(f"{self.img_dir}",f"{image_id}.jpeg")
         img = PIL.Image.open(img_path).convert("RGB")
 
-        # Convert to torchvision tensors
         img = tv_tensors.Image(img, dtype=torch.uint8)
 
         wt, ht = img.shape[-1], img.shape[-2]  # Get width and height
