35040b24e914fc2130d0fcaeee76e43e8fb14940
maximusjv <maksimus.vinokur@gmail.com>
2024-11-08 23:18:12 +0200

    many predictor optimizations

--- predictor.py
+++ predictor.py
@@ -1,8 +1,9 @@
 # Standard Library
 import csv
+import math
 import os
 import time
-from typing import Optional, Dict, Any
+from typing import Optional
 
 # Third-party Libraries
 import PIL
@@ -15,27 +16,29 @@ from torch import Tensor
 from torch.utils.data import Dataset, DataLoader, Subset
 
 # Torchvision
-import torchvision.tv_tensors as tv_tensors
 from torchvision.models.detection import FasterRCNN
 import torchvision.transforms.v2.functional as F
 from torchvision.ops import batched_nms
 
 from aleket_dataset import AleketDataset, collate_fn
 from dataset_statisics import visualize_bboxes
-from metrics import LOSSES_NAMES, VALIDATION_METRICS, Evaluator
+from metrics import Evaluator
 from utils import make_patches
 
 class Pacther(Dataset): 
     def __init__(
         self,
         images: list[str | Image | torch.Tensor] | Dataset,
+        size_factor: float,
         patch_size: int,
         patch_overlap: float,
         ):
         self.images = images
+        self.size_factor = size_factor
         self.patch_size = patch_size
         self.patch_overlap = patch_overlap
-    
+        
+    @torch.no_grad()
     def preprocess(self, image: Image | torch.Tensor | str, idx: int) -> tuple[list[list[int]], torch.Tensor]:
         if isinstance(image, str):
             image = PIL.Image.open(image)
@@ -44,9 +47,13 @@ class Pacther(Dataset):
             image = F.to_image(image)
     
         ht, wd =  image.shape[-2:]
+        ht = int(ht * self.size_factor)
+        wd = int(wd * self.size_factor)
+        
         padded_width, padded_height, patches = make_patches(
             wd, ht, self.patch_size, self.patch_overlap
         )
+        
         image = F.resize(image, size=(ht, wd))
         image = F.pad(image, padding=[0, 0, padded_width-wd, padded_height-ht],fill=0.0)
         image = F.to_dtype(image,  dtype=torch.float32, scale=True)
@@ -59,10 +66,12 @@ class Pacther(Dataset):
     def __len__(self):
         """Returns the total number of images in the dataset."""
         return len(self.images)
-    
+    @torch.no_grad()
     def postprocess(self,
                     patches: list[list[int]],
-                    predictions: list[dict[str, torch.Tensor]]
+                    predictions: list[dict[str, torch.Tensor]],
+                    nms_thresh: float,
+                    max_detections: int,
     ) -> dict[str, torch.Tensor]:
         
         boxes = []
@@ -77,22 +86,37 @@ class Pacther(Dataset):
                 boxes.append(prediction["boxes"])
                 labels.append(prediction["labels"])
                 scores.append(prediction["scores"])
-           
+        
+          
         labels = torch.cat(labels, dim=0)
         boxes = torch.cat(boxes, dim=0)
         scores = torch.cat(scores, dim=0)
         
+        boxes /= self.size_factor
+        keep = batched_nms(boxes, scores, labels, nms_thresh)
+
+        boxes = boxes[keep]
+        labels = labels[keep]
+        scores = scores[keep]
+
+        boxes = boxes[:max_detections]
+        scores = scores[:max_detections]
+        labels = labels[:max_detections]
+
         return {
             "boxes": boxes.cpu(),
             "scores": scores.cpu(),
             "labels": labels.cpu(),
         }
-        
+    @torch.no_grad()   
     def __getitem__(self, idx: int) -> tuple[list[list[int]], torch.Tensor]:
         if isinstance(self.images, Dataset):
             image, target = self.images[idx]
             return self.preprocess(image, target["image_id"])
-        return self.preprocess(self.images[idx], idx)
+        print(self.images[idx])
+        r = self.preprocess(self.images[idx], idx)
+
+        return r
         
 
 class Predictor:
@@ -100,19 +124,23 @@ class Predictor:
         self,
         model: FasterRCNN,
         device: torch.device,
-        detections_per_patch: int = 100,
         images_per_batch: int = 4,
+        image_size_factor: float = 0.8,
+        per_image_detections: int = 300,
+        
+        detections_per_patch: int = 100,
+        patches_per_batch: int = 4,
         patch_size: int = 1024,
         patch_overlap: float = 0.2,
-        patches_per_batch: int = 4,
     ):
 
         self.device = device
-        self.classes = list(AleketDataset.NUM_TO_CLASSES.values())
-        self.classes = self.classes[1:] # remove background class
+        self.classes = list(AleketDataset.NUM_TO_CLASSES.values())[1:] # remove background class
         self.model = model.to(device)
         self.images_per_batch = images_per_batch
-        
+        self.image_size_factor = image_size_factor
+        self.per_image_detections = per_image_detections
+
         self.patch_size = patch_size
         self.patch_overlap = patch_overlap
         self.detections_per_patch = detections_per_patch
@@ -133,7 +161,7 @@ class Predictor:
         self.model.roi_heads.detections_per_img = self.detections_per_patch
         self.model.eval()
      
-        patcher = Pacther(images, self.patch_size, self.patch_overlap)
+        patcher = Pacther(images, self.image_size_factor, self.patch_size, self.patch_overlap)
         dataloader = DataLoader(patcher,  
                                 batch_size=self.images_per_batch,
                                 num_workers=self.images_per_batch,
@@ -148,10 +176,12 @@ class Predictor:
                 for b_imgs in torch.split(imgs, self.patches_per_batch):
                     b_imgs = b_imgs.to(self.device)
                     predictions.extend(self.model(b_imgs))
-                predictions = patcher.postprocess(patches, predictions)
+                predictions = patcher.postprocess(patches, predictions, nms_thresh, self.per_image_detections)
                 result[idx] = predictions
+        
     
-        torch.cuda.empty_cache()
+        del patcher
+        del dataloader
         return result
 
     @torch.no_grad()
@@ -174,28 +204,30 @@ class Predictor:
               prediction: dict[str, Tensor],
               ) -> dict:
        
-        bboxes = prediction["boxes"].cpu().numpy()
-        labels = prediction["labels"].cpu().numpy()
+        bboxes = prediction["boxes"].numpy()
+        labels = prediction["labels"].numpy()
+        labels_names = [self.classes[i-1] for i in labels]
         count = {}
         area = {}
         for class_id, class_name in enumerate(self.classes):  # skips background
-            bboxes_by_class = bboxes[np.where(labels == class_id)]
+            bboxes_by_class = bboxes[np.where(labels == class_id+1)]
             count[class_name] = len(bboxes_by_class)
             area[class_name] = np.sum(
-                (bboxes_by_class[:, 2] - bboxes_by_class[:, 0])
-                * (bboxes_by_class[:, 3] - bboxes_by_class[:, 1])
-                )
+                (bboxes_by_class[:, 2] - bboxes_by_class[:, 0]) / 2.0
+                * (bboxes_by_class[:, 3] - bboxes_by_class[:, 1]) / 2.0
+                ) * math.pi # area of eclipse inside the rectangle
 
+       
         return {
             "bboxes": bboxes.tolist(),
-            "labels": labels.tolist(),
+            "labels": labels_names,
             "count": count,
             "area": area,
         }
 
     @torch.no_grad()
     def infer(self,
-              images: list[str | Image | torch.Tensor] | Dataset,
+              images: list[str | Image | torch.Tensor],
               output_dir: str,
               nms_thresh: float,
               score_thresh: float,
@@ -216,19 +248,19 @@ class Predictor:
             os.makedirs(annotated_dir, exist_ok=True)
 
 
-
-        predictions = self.get_predictions(images, nms_thresh, score_thresh)
-        with (open(stats_file_path, "w") as stats_file):
-            stats_writer = csv.writer(stats_file, delimiter="")
+        
+        with open(stats_file_path, "w", newline='') as stats_file:
+            stats_writer = csv.writer(stats_file, delimiter=",")
             headers = ["Image"]
             headers.extend([f"{class_name} area" for class_name in self.classes])
             headers.extend([f"{class_name} count" for class_name in self.classes])
             stats_writer.writerow(headers)
             
+            predictions = self.get_predictions(images, nms_thresh, score_thresh)
             for idx, pred in predictions.items():
                 image = images[idx]
                 image_name = os.path.basename(image) if isinstance(image, str) else str(idx)
-                stats = self.analyze(pred, nms_thresh, score_thresh)
+                stats = self.analyze(pred)
 
                 area = stats["area"]
                 count = stats["count"]
@@ -238,19 +270,25 @@ class Predictor:
                 row = [image_name]
                 row.extend([area[class_name] for class_name in self.classes])
                 row.extend([count[class_name] for class_name in self.classes])
-                stats_writer.writerows(row)
+                stats_writer.writerow(row)
 
                 if save_bboxes:
                     bboxes_file_path = os.path.join(bboxes_dir, f"{image_name}.csv")
-                    with open(bboxes_file_path, "w") as bboxes_file:
-                        bboxes_writer = csv.writer(bboxes_file, delimiter="")
+                    with open(bboxes_file_path, "w", newline='') as bboxes_file:
+                        bboxes_writer = csv.writer(bboxes_file, delimiter=",")
                         headers = ["xmin", "ymin", "xmax", "ymax", "class name"]
                         bboxes_writer.writerow(headers)
-                        rows = [bbox + [class_name] for bbox, class_name in zip(bboxes, labels)]
-                        bboxes_writer.writerows(rows)
+                        for (x1, y1, x2, y2), class_name in zip(bboxes, labels):
+                            bboxes_writer.writerow([int(x1), int(y1), int(x2), int(y2), class_name])
+
                 
                 if save_annotated_images:
-                    annotated_image_path = os.path.join(annotated_dir, f"{image_name}_annotated.png")
+                    annotated_image_path = os.path.join(annotated_dir, f"{image_name}_annotated.jpeg")
+                    if isinstance(image, str):
+                        image = PIL.Image.open(image)
+                    if isinstance(image, torch.Tensor):
+                        image = F.to_pil_image(image)
+
                     visualize_bboxes(image, bboxes, labels, save_path=annotated_image_path)
 
                         
